917fc51f792245b3dc790963c48222ff60b284e7
tarang <tarangsinghal2@gmail.com>
2020-11-06 12:56:48 +0530

    x

--- server.js
+++ server.js
@@ -22,9 +22,7 @@ app.use(todosRoutes);
 //production environment rules
 if(process.env.NODE_ENV==="production"){
     app.use(express.static("client/build"));
-    app.get("/",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    });
+    app.use('*', express.static('client/build')); // Added this  
 }
 
 const PORT = process.env.PORT;
