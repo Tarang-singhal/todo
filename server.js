1f82c40222a4005a3c748171b07fb54a87a390bc
tarang <tarangsinghal2@gmail.com>
2020-11-06 12:43:09 +0530

    x

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(todosRoutes);
 //production environment rules
 if(process.env.NODE_ENV==="production"){
     app.use(express.static("client/build"));
-    app.get("*",(req,res)=>{
+    app.get("/",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     });
 }
